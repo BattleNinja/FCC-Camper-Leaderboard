3281e97c8cec9a8fc9293f1f1b68da7360ad25c2
BattleNinja <mucherzhang@gmail.com>
2017-04-17 20:26:17 -0400

    get all data from API

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
+  constructor(){
+    super();
+    this.fetchData();  //write this function into constructor, then it can be called at very beginning.
+    this.state={
+      alltime:[],
+      recent:[],
+      curentstatus:"alltime"
+    };
+  }
+  get30daysdata() {
+      return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/recent');
+  }
+  getalldata() {
+      return axios.get('https://fcctop100.herokuapp.com/api/fccusers/top/alltime');
+  }
+  fetchData(){
+    axios.all([this.get30daysdata(),this.getalldata()])
+        .then(axios.spread((recent,alltime)=>{
+          this.setState({
+              recent:recent.data,
+              alltime:alltime.data
+          });
+        }));
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  changstatus(status){
+    this.setState({
+        curentstatus:status
+    });
+    console.log(this.state.alltime)
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div className="text-center">
+        <h1>Camper Board List</h1>
+        <button onClick={()=>this.changstatus("recent")} className="btn btn-primary">30days</button>
+        <button onClick={()=>this.changstatus("alltime")} className="btn btn-primary">all</button>
+
+      </div>
     );
   }
 }
